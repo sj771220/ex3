a8b3a48551119003a84a1f91ed27870240425c83
sj771220 <psw3758@gmail.com>
2023-09-18 10:33:04 +0900

    make project

---
+++ src/test/java/org/aerock/ex3/Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aerock.ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
